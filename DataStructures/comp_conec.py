8449d079b00e3e545221e59299a1e17d9458573e
unknown <n.caicedo@uniandes.edu.co>
2020-04-21 22:21:59 -0500

    data structure cc

--- DataStructures/comp_conec.py
+++ DataStructures/comp_conec.py
@@ -9,27 +9,27 @@ from datetime import datetime
 
 
 
-def depth_first_search(catalog,node):
+def depth_first_search(Graph, Mapa_de_marcar, node):
     valor={'nodo':node, 'stado':True, 'predecesor':None}
-    map.put(catalog['visitedMap'],valor['nodo'],valor)
-    list_ad=g.adjacents(catalog['delayGraph'],node)
+    map.put(Mapa_de_marcar,valor['nodo'],valor)
+    list_ad=g.adjacents(Graph,node)
     for i in range (1,lt.size(list_ad)+1):
         li_node=lt.getElement(list_ad,i)
-        if not map.contains(catalog['visitedMap'],li_node):
+        if not map.contains(Mapa_de_marcar,li_node):
             record={'nodo':li_node, 'stado':True, 'predecesor':node}
-            map.put(catalog['visitedMap'],record['nodo'],record)
-            depth_first_search(catalog,li_node)
+            map.put(Mapa_de_marcar,record['nodo'],record)
+            depth_first_search(Graph, Mapa_de_marcar, li_node)
             
 
-def countConnectedComponents (catalog):
+def countConnectedComponents (Graph, Mapa_de_marcar):
     """
     Retorna la cantidad de componentes conectados del grafo de revisiones
     """
     counter=0
-    list_nodes=g.vertices(catalog['delayGraph'])
+    list_nodes=g.vertices(Graph)
     for i in range(1,lt.size(list_nodes)+1):
         node=lt.getElement(list_nodes,i)
-        if not map.contains(catalog['visitedMap'],node):
-            depth_first_search(catalog,node)
+        if not map.contains(Mapa_de_marcar, node):
+            depth_first_search(Graph, Mapa_de_marcar, node)
             counter+=1
     return counter
